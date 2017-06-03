84ee65235e5e93ff5f20a725da781d70baf9720c
alan <alan.marazzi@gmail.com>
2017-06-03 16:29:56 +0200

    Packaging structure

---
+++ mepcheck/__init__.py
@@ -0,0 +1,1 @@
+from mepcheck import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="mepcheck",
+      version="0.1",
+      description="Retrieve voting data for all MEPs and return them in a structured way",
+      url="https://github.com/alanmarazzi/mepcheck",
+      author="Alan Marazzi",
+      author_email="alan.marazzi@gmail.com",
+      license="MIT",
+      packages=["mepcheck"],
+      zip_safe=False)
